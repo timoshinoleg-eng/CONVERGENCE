f3251a5fa480b1f81c11e4670bb7407fe5d5177e
timoshinoleg-eng <timoshin.oleg@gmail.com>
2026-09-17 10:46:13 +0300

    spike: include donor bundle probe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import { runDonorBundleProbe } from "./game/spike/bundle-probe";
 import "./style.css";
 
+void runDonorBundleProbe();
 createApp(App).use(createPinia()).mount("#app");
